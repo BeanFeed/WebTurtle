fb852c7b4c9359ab9c92bf402e261bacb3b1af65
Austin Dean <two.austin.two@gmail.com>
2022-01-03 08:44:19 -0500

    Added debug lines

--- websocket.js
+++ websocket.js
@@ -3,9 +3,12 @@ const ws = require('ws');
 const wss = new ws.Server({port:80});
 
 wss.on('Connection', function(wsss){
+    console.log('connected');
     wsss.on('message',function(message){
+        console.log(message);
         if (message == 'f') {
             wss.send(message);
+            
         }else if (message == 'b'){
             wss.send(message);
         }else if (message == 'l'){
@@ -13,6 +16,5 @@ wss.on('Connection', function(wsss){
         }else if (message == 'r'){
             wss.send(message);
         }else {return;}
-        console.log(message);
     });
 })
