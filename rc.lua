1623e4eebb575070ab9a1eb913028c967c033274
CrackedLua <86930720+CrackedLua@users.noreply.github.com>
2022-01-04 08:28:15 -0500

    program for turtle

---
+++ rc.lua
@@ -0,0 +1,25 @@
+os.loadAPI("wsmsg.lua")
+local tARGs = {...}
+local url = tARGs[1]
+local pass = tARGs[2]
+local ws,err = http.websocket(url)
+turtle.select(1)
+
+if ws then
+    print("Connected To: "..url)
+    print(os.getComputerID())
+    print(pass)
+    json
+end
+
+while ws do
+
+    local args, x = ws.receive()
+    print("ID: "..args[id].." Pass: "..args[pass])
+    if args.id == os.getComputerID() and args.pass == pass then
+        
+        print(args[cmd])
+        local cmd = loadstring(args.cmd)
+        cmd()
+    end
+end
