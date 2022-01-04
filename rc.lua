e527b032b0a8a6e2193d5eae6a7879da5bf75e63
CrackedLua <86930720+CrackedLua@users.noreply.github.com>
2022-01-04 15:50:58 -0500

    Ws args converted to lua

--- rc.lua
+++ rc.lua
@@ -3,23 +3,24 @@ local url = tARGs[1]
 local pass = tARGs[2]
 local ws,err = http.websocket(url)
 turtle.select(1)
-
+local id = tostring(os.getComputerID())
 if ws then
     print("Connected To: "..url)
     print(os.getComputerID())
     print(pass)
-    json
 end
 
 while ws do
 
     local args, x = ws.receive()
-    args = textutils.unserialise(args)
-    print("ID: "..args[id].." Pass: "..args[pass])
-    if args.id == os.getComputerID() and args.pass == pass then
-        
-        print(args[cmd])
-        local cmd = loadstring(args.cmd)
-        cmd()
+    print(args)
+    args = textutils.unserialiseJSON(args)
+    print(args)
+    print(textutils.serialise(args))
+    print(" ")
+    print("ID: "..args.id.." Pass: "..args.pass.." Cmd: "..args.cmd)
+    if args.id == id and args.pass == pass then
+        local func = loadstring(args.cmd)
+        func()
     end
 end
