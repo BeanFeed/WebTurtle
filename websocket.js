af6b3d780323816e745e29ceaa1462e28d4815fb
Austin Dean <two.austin.two@gmail.com>
2022-01-04 07:18:12 -0500

    sends array for transer of id and pass

--- websocket.js
+++ websocket.js
@@ -6,21 +6,8 @@ wss = new websocket.Server({ port:8123 });
 wss.on('connection', function connection(ws){
     console.log('connected');
     ws.on('message',function(msg){
-        console.log(msg.toString());
-        let message = msg.toString();
-        if (message == 'f') {
-            wss.broadcast("turtle.forward()");
-        }else if (message == 'b'){
-            wss.broadcast("turtle.back()");
-        }else if (message == 'l'){
-            wss.broadcast("turtle.turnLeft()");
-        }else if (message == 'r'){
-            wss.broadcast("turtle.turnRight()");
-        }else if (message == 'u') {
-            wss.broadcast("turtle.up()");
-        }else if (message == 'd') {
-            wss.broadcast("turtle.down()")
-        }else {return;}
+        console.log(JSON.parse(msg));
+        wss.broadcast(msg);
     });
 });
 
