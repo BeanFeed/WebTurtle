e9232d47ce098a94d2ad384cbbd3fc496dd471f3
Austin Dean <two.austin.two@gmail.com>
2022-01-09 17:47:15 -0500

    Relays Turtle Inventory

--- rc.lua
+++ rc.lua
@@ -10,15 +10,39 @@ if ws then
     print(pass)
 end
 
-while ws do
 
+
+function RecCMD()
     local args, x = ws.receive()
     args = textutils.unserialiseJSON(args)
     if args.id == id and args.pass == pass then
         print(textutils.serialise(args))
         print(" ")
         print("ID: "..args.id.." Pass: "..args.pass.." Cmd: "..args.cmd)
+        if args.extra ~= nil then
+            
+        end
         local func = loadstring(args.cmd)
         func()
     end
+end
+print(err)
+
+function Inv()
+    while ws do
+    os.sleep(5)
+    local inv = {}
+    for i = 1, 16 do
+        inv["slot" .. i] = turtle.getItemDetail(i)
+    end
+    inv.fromWss = false
+    inv.log = false
+    --print(textutils.serialise(inv))
+    inv = textutils.serialiseJSON(inv)
+    ws.send(inv)
+    end
+end
+
+while ws do
+    parallel.waitForAll(RecCMD,Inv)
 end
